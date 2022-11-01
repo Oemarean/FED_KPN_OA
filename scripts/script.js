9a10c36665b80709b144353cea03392516b78609
Oemarean <oemaralkatiri@gmail.com>
2022-11-01 19:03:08 +0100

    v6

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
-console.log("hi");
+
+// Open
+var menuOpenButton = document.querySelector("header > button");
+menuOpenButton.addEventListener("click" , menuOpenen);
+function menuOpenen(){
+  var deNav = document.querySelector("header nav");
+  deNav.classList.add("open")
+}
+
+// Close
+var sluitenButton = document.querySelector("header nav button");
+sluitenButton.addEventListener("click" , menuSluiten)
+function menuSluiten(){
+  var deNav = document.querySelector("header nav");
+  deNav.classList.remove("open")
+}
