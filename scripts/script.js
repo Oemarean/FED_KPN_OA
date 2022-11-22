f59cb2fa2c5e6b9daa7335a74bdea6331a716f76
Oemarean <oemaralkatiri@gmail.com>
2022-11-22 14:37:46 +0100

    R10?

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,38 @@
 // JavaScript Document
+console.log("hi");
+
+// NAVIGATIE DEEL
 
 // Open
-var menuOpenButton = document.querySelector("header > button");
-menuOpenButton.addEventListener("click" , menuOpenen);
-function menuOpenen(){
-  var deNav = document.querySelector("header nav");
-  deNav.classList.add("open")
-}
+  var menuOpenButton = document.querySelector("header button:nth-of-type(2)");
+
+  menuOpenButton.addEventListener("click" , menuOpenen);
+
+  function menuOpenen(){
+    var deNav = document.querySelector("header nav");
+    deNav.classList.add("open")
+ }
 
 // Close
-var sluitenButton = document.querySelector("header nav button");
-sluitenButton.addEventListener("click" , menuSluiten)
-function menuSluiten(){
-  var deNav = document.querySelector("header nav");
-  deNav.classList.remove("open")
+  var sluitenButton = document.querySelector("header nav:nth-of-type(2) button");
+
+  sluitenButton.addEventListener("click" , menuSluiten);
+
+  function menuSluiten(){
+    var deNav = document.querySelector("header nav");
+    deNav.classList.remove("open")
 }
 
+// FOOTER DEEL
 
 // footermenu uitklap
-var footerButton = document.querySelector("footer button");
-// var deHeader = document.querySelector("section:nth-of-type(1) h2");
+var footerButton1 = document.querySelector("footer section:nth-of-type(1) button");
+var footerKopje1 = document.querySelector("footer section:nth-of-type(1) h2");
 
-footerButton.addEventListener("click", toggleMenu);
-// deHeader.addEventListener("click", toggleMenu);
+footerButton1.addEventListener("click", toggleMenu);
+footerKopje1.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-	var hetMenu = document.querySelector("footer ul");
-	hetMenu.classList.toggle("open");
+	var hetMenu = document.querySelector("footer section:nth-of-type(1) ul");
+	hetMenu.classList.toggle("uitklap");
 }
