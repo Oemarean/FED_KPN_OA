c8bc9fe7a0e89def224a86c7d548fb75b45fc2a3
Oemarean <oemaralkatiri@gmail.com>
2022-11-24 12:11:42 +0100

    v12

--- scripts/script.js
+++ scripts/script.js
@@ -27,13 +27,54 @@ var menuOpenButton = document.querySelector("header nav ul li:nth-of-type(5) but
 // FOOTER DEEL
 
 // footermenu uitklap
-var footerButton1 = document.querySelector("footer section button");
-var footerKopje1 = document.querySelector("footer section h2");
+var footerButton1 = document.querySelector("footer section:nth-of-type(1) button");
+var footerKopje1 = document.querySelector("footer section:nth-of-type(1) h2");
+
+var footerButton2 = document.querySelector("footer section:nth-of-type(2) button");
+var footerKopje2 = document.querySelector("footer section:nth-of-type(2) h2");
+
+var footerButton3 = document.querySelector("footer section:nth-of-type(3) button");
+var footerKopje3 = document.querySelector("footer section:nth-of-type(3) h2");
+
+var footerButton4 = document.querySelector("footer section:nth-of-type(4) button");
+var footerKopje4 = document.querySelector("footer section:nth-of-type(4) h2");
+
+var footerButton5 = document.querySelector("footer section:nth-of-type(5) button");
+var footerKopje5 = document.querySelector("footer section:nth-of-type(5) h2");
+
 
 footerButton1.addEventListener("click", toggleMenu);
 footerKopje1.addEventListener("click", toggleMenu);
+footerButton2.addEventListener("click", toggleMenu);
+footerKopje2.addEventListener("click", toggleMenu);
+footerButton3.addEventListener("click", toggleMenu);
+footerKopje3.addEventListener("click", toggleMenu);
+footerButton4.addEventListener("click", toggleMenu);
+footerKopje4.addEventListener("click", toggleMenu);
+footerButton5.addEventListener("click", toggleMenu);
+footerKopje5.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-	var hetMenu = document.querySelector("footer section ul");
-	hetMenu.classList.toggle("uitklap");
+	var hetMenu1 = document.querySelector("footer section:nth-of-type(1) ul");
+	hetMenu1.classList.toggle("uitklap1");
+}
+
+function toggleMenu() {
+	var hetMenu2 = document.querySelector("footer section:nth-of-type(2) ul");
+	hetMenu2.classList.toggle("uitklap2");
+}
+
+function toggleMenu() {
+	var hetMenu3 = document.querySelector("footer section:nth-of-type(3) ul");
+	hetMenu3.classList.toggle("uitklap3");
+}
+
+function toggleMenu() {
+	var hetMenu4 = document.querySelector("footer section:nth-of-type(4) ul");
+	hetMenu4.classList.toggle("uitklap4");
+}
+
+function toggleMenu() {
+	var hetMenu5 = document.querySelector("footer section:nth-of-type(5) ul");
+	hetMenu5.classList.toggle("uitklap5");
 }
